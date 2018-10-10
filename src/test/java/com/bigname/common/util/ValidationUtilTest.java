ac6bcf49b5a424b89d5e6c829f2c10e2d1835fab
Manu NarayanaPrasad <mnprasad78@gmail.com>
2018-10-09 21:39:43 -0400

    Adding sample junit4 test case for validation util and making the test cases to pass

---
+++ src/test/java/com/bigname/common/util/ValidationUtilTest.java
@@ -0,0 +1,39 @@
+package com.bigname.common.util;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Manu V NarayanaPrasad (manu@blacwood.com)
+ * @since 1.0
+ */
+public class ValidationUtilTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void isNull() throws Exception {
+    }
+
+    @Test
+    public void isNotNull() throws Exception {
+    }
+
+    @Test
+    public void isEmpty() throws Exception {
+        assertTrue(ValidationUtil.isEmpty(""));
+    }
+
+    @Test
+    public void isNotEmpty() throws Exception {
+    }
+
+}
--- src/test/java/com/bigname/pimclient/PimClientApplicationTests.java
+++ src/test/java/com/bigname/pimclient/PimClientApplicationTests.java
@@ -1,12 +1,15 @@
 package com.bigname.pimclient;
 
+import com.bigname.pim.PimApplication;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ContextConfiguration(classes={PimApplication.class})
 public class PimClientApplicationTests {
 
 	@Test
