1b91d6391b1f3960f4f5263f76957cc2ea2babea
Manu NarayanaPrasad <mnprasad78@gmail.com>
2018-11-06 13:49:25 -0500

    Attribute Collection Crreation from Feed

--- src/main/java/com/bigname/pim/data/loader/ProductLoader.java
+++ src/main/java/com/bigname/pim/data/loader/ProductLoader.java
@@ -1,18 +1,19 @@
 package com.bigname.pim.data.loader;
 
-import com.bigname.pim.api.domain.Attribute;
-import com.bigname.pim.api.domain.AttributeCollection;
-import com.bigname.pim.api.domain.AttributeGroup;
-import com.bigname.pim.api.domain.ValidatableEntity;
+import com.bigname.common.util.ConversionUtil;
+import com.bigname.common.util.ValidationUtil;
+import com.bigname.pim.api.domain.*;
 import com.bigname.pim.api.service.AttributeCollectionService;
 import com.bigname.pim.api.service.FamilyService;
 import com.bigname.pim.api.service.ProductService;
 import com.bigname.pim.api.service.ProductVariantService;
+import com.bigname.pim.util.ConvertUtil;
 import com.bigname.pim.util.FindBy;
 import com.bigname.pim.util.POIUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -41,25 +42,57 @@ public class ProductLoader {
         List<List<String>> data = POIUtil.readData(filePath);
         System.out.println("size-------------->"+data.size());
         System.out.println("size-------------->"+data.get(0).size());
+        List<String> availableAttributeTypes = Arrays.asList("INPUTBOX", "TEXTAREA", "DROPDOWN", "YES_NO");
+        attributeCollectionService.get(attributeCollectionId, FindBy.EXTERNAL_ID, false).ifPresent(attributeCollection -> {
 
-        AttributeGroup defaultGroup = new AttributeGroup();
-        defaultGroup.setId("DEFAULT_GROUP");
+            attributeCollection.setGroup("ATTRIBUTES");
 
-        for (int i=0;i<data.get(0).size();i++){
-            if("IGNORE".equals(data.get(0).get(i))){
-                continue;
+            AttributeGroup defaultGroup = new AttributeGroup();
+            defaultGroup.setId("DEFAULT_GROUP");
+
+            List<String> attributeNames = data.get(0);
+            List<String> attributeTypes = data.get(1);
+            List<String> familyAttributeGroup = data.get(2);
+            List<String> familyAttributeSubgroup = data.get(3);
+
+            List<List<String>> variantsData = data.subList(4, data.size());
+
+            for (int i = 1; i < attributeNames.size(); i++) {
+                if (!availableAttributeTypes.contains(attributeTypes.get(i))) {
+                    continue;
+                }
+                String attributeName = attributeNames.get(i);
+                Attribute attribute = new Attribute();
+                attribute.setAttributeGroup(AttributeGroup.getDefaultGroup());
+                attribute.setUiType(Attribute.UIType.get(attributeTypes.get(i)));
+                attribute.setName(attributeName);
+                System.out.println("Attribute---> " + attribute.toString());
+                if(!attributeCollection.getAttributes().containsKey(AttributeGroup.DEFAULT_GROUP_ID)
+                        || attributeCollection.getAttributes().get(AttributeGroup.DEFAULT_GROUP_ID).getAttributes().isEmpty()
+                        || !attributeCollection.getAttributes().get(AttributeGroup.DEFAULT_GROUP_ID).getAttributes().containsKey(ValidatableEntity.toId(attribute.getName()))) {
+                    attributeCollection.addAttribute(attribute);
+                }
+                attribute = attributeCollection.getAttributes().get(AttributeGroup.DEFAULT_GROUP_ID).getAttributes().get(ValidatableEntity.toId(attribute.getName()));
+                for (int j = 0; j < variantsData.size(); j++) {
+                    if(!ConvertUtil.toBoolean(attribute.getSelectable())) {
+                        continue;
+                    }
+                    String attributeOptionValue = variantsData.get(j).get(i);
+                    if (ValidationUtil.isNotEmpty(attributeOptionValue) && !attribute.getOptions().containsKey(ValidatableEntity.toId(attributeOptionValue))) {
+                        AttributeOption attributeOption = new AttributeOption();
+                        attributeOption.setCollectionId(attributeCollectionId);
+                        attributeOption.setAttributeId(attribute.getFullId());
+                        attributeOption.setValue(attributeOptionValue);
+                        attributeOption.setActive("Y");
+                        attributeOption.orchestrate();
+                        attribute.getOptions().put(ValidatableEntity.toId(attributeOptionValue), attributeOption);
+                    }
+                }
+
+
             }
-            Attribute attribute = new Attribute();
-            attribute.setAttributeGroup(AttributeGroup.getDefaultGroup());
-            attribute.setUiType(Attribute.UIType.get(data.get(0).get(i)));
-            attribute.setName(data.get(1).get(i));
-            System.out.println("Attribute---> "+attribute.toString());
-            createAttribute(attribute);
-           /* for (int j=0;j<2;j++){
-                System.out.println("Data value : "+data.get(j).get(i));
-            }*/
-
-        }
+            attributeCollectionService.update(attributeCollectionId, FindBy.EXTERNAL_ID, attributeCollection);
+        });
 
 
 
