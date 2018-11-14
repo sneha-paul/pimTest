bdcda9041ba311f33abd768922e410336673bee8
Manu NarayanaPrasad <mnprasad78@gmail.com>
2018-11-13 23:27:18 -0500

    Custom Variant Service & Pricing Details - WIP

--- src/main/java/com/bigname/pim/api/service/ProductService.java
+++ src/main/java/com/bigname/pim/api/service/ProductService.java
@@ -18,6 +18,18 @@ import java.util.Optional;
  * Created by sruthi on 19-09-2018.
  */
 public interface ProductService extends BaseService<Product, ProductDAO> {
+    /**
+     * Method to get variants of a product in paginated format.
+     *
+     * @param productId Internal or External id of the Product
+     * @param findBy Type of the product id, INTERNAL_ID or EXTERNAL_ID
+     * @param channelId
+     * @param page
+     * @param size
+     * @param sort
+     * @param activeRequired
+     * @return
+     */
     Page<ProductVariant> getProductVariants(String productId, FindBy findBy, String channelId, int page, int size, Sort sort, boolean... activeRequired);
     List<ProductVariant> getProductVariants(String productId, FindBy findBy, String channelId, Sort sort, boolean... activeRequired);
     Optional<ProductVariant> getProductVariant(String productId, FindBy productIdFindBy, String channelId, String productVariantId, FindBy variantIdFindBy, boolean... activeRequired);
