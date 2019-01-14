f4976c4f486f17e04a8d75407b68a39e905f15f9
sanoop9745 <sanoop.p@e-xposures.com>
2019-01-14 11:26:47 +0000

    Search Option for User Data Table

--- src/main/java/com/bigname/pim/client/web/controller/RegistrationController.java
+++ src/main/java/com/bigname/pim/client/web/controller/RegistrationController.java
@@ -1,22 +1,31 @@
 package com.bigname.pim.client.web.controller;
 
+import com.bigname.common.datatable.model.Pagination;
+import com.bigname.common.datatable.model.Request;
+import com.bigname.common.datatable.model.Result;
+import com.bigname.common.datatable.model.SortOrder;
 import com.bigname.pim.api.domain.User;
 import com.bigname.pim.api.exception.EntityNotFoundException;
 import com.bigname.pim.api.service.UserService;
 import com.bigname.pim.client.model.Breadcrumbs;
 import com.bigname.pim.util.FindBy;
+import com.bigname.pim.util.Pageable;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.*;
+
+import static com.bigname.common.util.ValidationUtil.isEmpty;
 
 
 /**
@@ -122,6 +131,34 @@ public class RegistrationController extends BaseController<User, UserService> {
         Map<String, Object> model = new HashMap<>();
         model.put("active", "USERS");
         return new ModelAndView("user/users", model);
+    }
+
+
+    @RequestMapping(value =  {"/list", "/data"})
+    @ResponseBody
+    @SuppressWarnings("unchecked")
+    public Result<Map<String, String>> all(HttpServletRequest request, HttpServletResponse response, Model model) {
+        Request dataTableRequest = new Request(request);
+        if(isEmpty(dataTableRequest.getSearch())) {
+            return super.all(request, response, model);
+        } else {
+            Pagination pagination = dataTableRequest.getPagination();
+            Result<Map<String, String>> result = new Result<>();
+            result.setDraw(dataTableRequest.getDraw());
+            Sort sort;
+            if(pagination.hasSorts()) {
+                sort = Sort.by(new Sort.Order(Sort.Direction.valueOf(SortOrder.fromValue(dataTableRequest.getOrder().getSortDir()).name()), dataTableRequest.getOrder().getName()));
+            } else {
+                sort = Sort.by(new Sort.Order(Sort.Direction.ASC, "externalId"));
+            }
+            List<Map<String, String>> dataObjects = new ArrayList<>();
+            List<User> paginatedResult = userService.findAll("userName", dataTableRequest.getSearch(), new Pageable(pagination.getPageNumber(), pagination.getPageSize(), sort), false);
+            paginatedResult.forEach(e -> dataObjects.add(e.toMap()));
+            result.setDataObjects(dataObjects);
+            result.setRecordsTotal(Long.toString(paginatedResult.size()));
+            result.setRecordsFiltered(Long.toString(paginatedResult.size()));
+            return result;
+        }
     }
 
 
