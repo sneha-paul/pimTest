d35bfc76d40cc05e4a3fd866e0afaf05cc1b28f6
sanoop9745 <sanoop.p@e-xposures.com>
2019-01-31 06:51:04 +0000

    Write To Excel CategoryDatas

--- src/main/java/com/bigname/pim/client/web/controller/ExportController.java
+++ src/main/java/com/bigname/pim/client/web/controller/ExportController.java
@@ -1,6 +1,7 @@
 package com.bigname.pim.client.web.controller;
 
 import com.bigname.pim.data.loader.exporter.CatalogExporter;
+import com.bigname.pim.data.loader.exporter.CategoryExporter;
 import com.bigname.pim.data.loader.exporter.WebsiteExporter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,10 +19,12 @@ import java.util.Map;
 public class ExportController {
     private CatalogExporter catalogExporter;
     private WebsiteExporter websiteExporter;
+    private CategoryExporter categoryExporter;
 
-    public ExportController( CatalogExporter catalogExporter, WebsiteExporter websiteExporter) {
+    public ExportController( CatalogExporter catalogExporter, WebsiteExporter websiteExporter,CategoryExporter categoryExporter) {
         this.catalogExporter = catalogExporter;
         this.websiteExporter = websiteExporter;
+        this.categoryExporter=categoryExporter;
     }
 
     @RequestMapping(value = "/catalogs" , method = RequestMethod.GET)
@@ -36,6 +39,12 @@ public class ExportController {
     public Map<String, Object> exportWebsiteData(HttpServletRequest request) {
         Map<String, Object> model = new HashMap<>();
         websiteExporter.exportData("/DevStudio/Docs/PIM_ExcelFiles/WebsiteData.xlsx");
+        return model;
+    }
+    @RequestMapping(value = "/categories", method = RequestMethod.GET)
+    public Map<String, Object> exportCategoryData(HttpServletRequest request) {
+        Map<String, Object> model = new HashMap<>();
+        categoryExporter.exportData("/DevStudio/Docs/PIM_ExcelFiles/CategoryData.xlsx");
         return model;
     }
 }
---
+++ src/main/java/com/bigname/pim/data/loader/exporter/CategoryExporter.java
@@ -0,0 +1,38 @@
+package com.bigname.pim.data.loader.exporter;
+
+import com.bigname.pim.api.domain.Category;
+import com.bigname.pim.api.service.CategoryService;
+import com.bigname.pim.util.POIUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.*;
+
+/**
+ * Created by sanoop on 31/01/2019.
+ */
+@Component
+public class CategoryExporter {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    public boolean exportData(String filepath){
+        List<Category> categoryData = categoryService.getAll(null,true);
+        Map<String,Object[]>data = new TreeMap<>();
+        data.put("1", new Object[]{"CATEGORY_ID", "CATEGORY_NAME","DESCRIPTION","ACTIVE", "DISCONTINUED" });
+
+        int i=2;
+        for (Iterator<Category> iter = categoryData.iterator(); iter.hasNext(); ) {
+            Category element = iter.next();
+            data.put(Integer.toString(i), new Object[]{element.getCategoryId(), element.getCategoryName(),element.getDescription(), element.getActive(),element.getDiscontinued() });
+            i++;
+        }
+        POIUtil.writeData(filepath,"category",data);
+
+       return true;
+    }
+
+
+
+}
