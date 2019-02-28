ae6f9b4afb09c8c6bc354807fbe42820577f2ce6
Sruthi Krishna <Sruthi.Krishna@e-xposures.com>
2019-02-28 17:27:38 +0530

    RegistrationServiceImplTest

--- src/test/java/com/bigname/pim/api/service/impl/RegistrationServiceImplTest.java
+++ src/test/java/com/bigname/pim/api/service/impl/RegistrationServiceImplTest.java
@@ -1,25 +1,95 @@
 package com.bigname.pim.api.service.impl;
 
+import com.bigname.common.util.CollectionsUtil;
+import com.bigname.pim.PimApplication;
+import com.bigname.pim.api.domain.User;
+import com.bigname.pim.api.persistence.dao.UserDAO;
+import com.bigname.pim.api.service.RegistrationService;
+import com.bigname.pim.api.service.UserService;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import static com.bigname.core.util.FindBy.EXTERNAL_ID;
 import static org.junit.Assert.*;
 
 /**
  * Created by sruthi on 23-02-2019.
  */
+@ActiveProfiles("test")
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes={PimApplication.class})
 public class RegistrationServiceImplTest {
+    @Autowired
+    private UserDAO userDAO;
+
+    @Qualifier("customUserService")
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    public PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private RegistrationService registrationService;
+
     @Before
     public void setUp() throws Exception {
-    }
-
-    @After
-    public void tearDown() throws Exception {
+        userDAO.getMongoTemplate().dropCollection(User.class);
     }
 
     @Test
     public void sendVerificationEmail() throws Exception {
+        List<Map<String, Object>> usersData = new ArrayList<>();
+        usersData.add(CollectionsUtil.toMap("name", "TestUser", "password", "test123", "externalId", "testUser@gmail.com", "email", "testUser@gmail.com", "active", "Y"));
+
+        usersData.forEach(userData -> {
+
+            User userDTO = new User();
+            userDTO.setUserName((String) userData.get("name"));
+            userDTO.setPassword(passwordEncoder.encode((String) userData.get("password")));
+            userDTO.setEmail((String) userData.get("email"));
+            userDTO.setActive((String) userData.get("active"));
+            userDTO.setStatus("Active");
+            userDTO.setAvatar(userDTO.getAvatar());
+            userDTO.setGroup("CREATE");
+            userDAO.insert(userDTO);
+
+            User newUser = userService.get(userDTO.getEmail(), EXTERNAL_ID, false).orElse(null);
+            Assert.assertTrue(newUser != null);
+            Assert.assertTrue(newUser.diff(userDTO).isEmpty());
+        });
+
+        User user = userService.get(usersData.get(0).get("email").toString(), EXTERNAL_ID, false).orElse(null);
+        String recipientAddress = user.getEmail();
+        String subject = "Registration Confirmation";
+        String message = "Hi "+  user.getUserName() + ",  Please login to the following link to confirm your registration : ";
+
+        registrationService.sendVerificationEmail(subject, recipientAddress, message); //TODO : returnType void, so didn;t write anyAssert
+
+        userDAO.getMongoTemplate().dropCollection(User.class);
     }
+
+    @After
+    public void tearDown() throws Exception {
+        userDAO.getMongoTemplate().dropCollection(User.class);
+    }
+
 
 }
