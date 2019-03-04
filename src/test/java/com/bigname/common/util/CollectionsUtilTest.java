106be3a443fd76e8c8ccb96a0971a19bef56afb7
Aswathy Sadanandan <aswathy.sadanandan@e-xposures.com>
2019-03-04 17:15:43 +0530

    CollectionsUtilTest-Inprogress

--- src/test/java/com/bigname/common/util/CollectionsUtilTest.java
+++ src/test/java/com/bigname/common/util/CollectionsUtilTest.java
@@ -6,55 +6,170 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.*;
+import java.util.stream.Stream;
+
 import static org.junit.Assert.*;
 
 /**
  * Created by sruthi on 27-02-2019.
  */
 public class CollectionsUtilTest {
-    @Before
-    public void setUp() throws Exception {
-    }
-
-    @After
-    public void tearDown() throws Exception {
-    }
 
     @Test
     public void filterMap() throws Exception {
+        Map<String, Object> input=new HashMap<>();
+        input.put("50",9);
+        input.put("25",6);
+        input.put("10",3);
+        List<String> keyList=new ArrayList<>();
+        keyList.add("50");
+        keyList.add("10");
+        Map<String, Object> output=new HashMap<>();
+        output.put("25",6);
+        Assert.assertEquals(CollectionsUtil.filterMap(input,keyList),output);
     }
 
     @Test
     public void toLinkedMap() throws Exception {
+        Stream<String[]>
+                str = Stream
+                .of(new String[][] { { "a", "GeeksForGeeks" },
+                        { "b", "geeks" },
+                        { "c", "Geeks" },
+                        { "d", "gks" },
+                        { "e", "GeeksName" },
+                        { "f", "GeeksPlace" }});
+        LinkedHashMap map=new LinkedHashMap();
+        map.put("a", "GeeksForGeeks");
+        map.put("b", "geeks");
+        map.put("c", "Geeks");
+        map.put("d", "gks");
+        map.put("e", "GeeksName");
+        map.put("f", "GeeksPlace");
+        Assert.assertEquals(str.collect( CollectionsUtil.toLinkedMap(p -> p[0], p -> p[1])),map);
     }
 
     @Test
     public void toTreeMap() throws Exception {
+        Stream<String[]>
+                str = Stream
+                .of(new String[][] { { "a", "GeeksForGeeks" },
+                        { "b", "geeks" },
+                        { "c", "Geeks" },
+                        { "d", "gks" },
+                        { "e", "GeeksName" },
+                        { "f", "GeeksPlace" }});
+        TreeMap map=new TreeMap();
+        map.put("a", "GeeksForGeeks");
+        map.put("b", "geeks");
+        map.put("c", "Geeks");
+        map.put("d", "gks");
+        map.put("e", "GeeksName");
+        map.put("f", "GeeksPlace");
+
+        Assert.assertEquals(str.collect( CollectionsUtil.toLinkedMap(p -> p[0], p -> p[1])),map);
     }
 
     @Test
     public void generifyMap() throws Exception {
+        Map<String, Object> input=new HashMap<>();
+        input.put("50",9);
+        input.put("25",6);
+        input.put("10",3);
+        Assert.assertEquals(CollectionsUtil.generifyMap(input),input);
     }
 
     @Test
     public void toMap() throws Exception {
-        /*Website website = new Website();
-        website.setWebsiteName("Envelope");
-        website.setWebsiteId("ENVELOPE");
-
-        Assert.assertEquals(CollectionsUtil.toMap(website), null);*/
+        Object[] obj= new Object[]{ "name", "Test1.com", "externalId", "TEST_1", "url", "www.test1.com", "active", "Y" };
+        Map<String, Object> map=new HashMap<>();
+        map.put("name","Test1.com");
+        map.put("externalId","TEST_1");
+        map.put("url","www.test1.com");
+        map.put("active","Y");
+        Assert.assertEquals(CollectionsUtil.toMap(obj),map);
     }
 
     @Test
     public void compareMaps() throws Exception {
+        Map<String, Object> map1=new HashMap<>();
+        map1.put("50",9);
+        map1.put("25",6);
+        map1.put("10",3);
+
+        Map<String, Object> map2=new HashMap<>();
+        map2.put("50",4);
+        map2.put("25",6);
+        map2.put("10",66);
+
+        Map<String, Object> out=new HashMap<>();
+        map2.put("50",9);
+        map2.put("10",3);
+        Assert.assertEquals(CollectionsUtil.compareMaps(map1,map2),out);
     }
 
     @Test
     public void flattenMap() throws Exception {
+        Map<String, Map<String, Object>> input = new HashMap<>();
+        Map<String, Object> objInput=new HashMap<>();
+        Map<String, Object> objInput1=new HashMap<>();
+        Map<String, Object> objInput2=new HashMap<>();
+
+        objInput.put("30",9);
+        objInput.put("45",6);
+        objInput.put("10",3);
+        input.put("plan",objInput);
+
+        objInput1.put("50",1);
+        objInput1.put("25",2);
+        objInput1.put("10",3);
+        input.put("map3",objInput1);
+
+        objInput2.put("50",3);
+        objInput2.put("25",77);
+        objInput2.put("20",6);
+        input.put("map2",objInput2);
+
+        Map<String, Object> map=new HashMap<>();
+        map.put("45",6);
+        map.put("25",77);
+        map.put("50",3);
+        map.put("30",9);
+        map.put("20",6);
+        map.put("10",3);
+        Assert.assertEquals(CollectionsUtil.flattenMap(input),map);
     }
 
     @Test
     public void buildMapString() throws Exception {
+       /* Map<String, Map<String, Object>> input = new HashMap<>();
+        Map<String, Object> objInput=new HashMap<>();
+        Map<String, Object> objInput1=new HashMap<>();
+
+        objInput.put("25",6);
+        input.put("plan",objInput);
+
+        objInput1.put("25",45);
+        input.put("map1",objInput1);
+
+        StringBuilder builder = new StringBuilder();
+        builder.append("25");
+        builder.append(",");
+        builder.append("0");
+        builder.append(",");
+        builder.append("45");
+        builder.append(";");
+        builder.append("25");
+        builder.append(",");
+        builder.append("1");
+        builder.append(",");
+        builder.append("6");
+        builder.append(";");
+
+       // StringBuilder builder = new StringBuilder("25,0,2;50,0,1;10,0,3;25,1,77;50,1,3;10,1,6;25,2,45;50,2,4;10,2,66;25,3,6;50,3,9;10,3,3;");
+    //    String out= "25,0,2;50,0,1;10,0,3;25,1,77;50,1,3;10,1,6;25,2,45;50,2,4;10,2,66;25,3,6;50,3,9;10,3,3;";
+        Assert.assertEquals(CollectionsUtil.buildMapString(input,0),builder);*/
     }
 
 }
