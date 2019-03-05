94d72309570956e4835021a87830ff2aa5367920
Manu NarayanaPrasad <mnprasad78@gmail.com>
2019-03-05 15:56:23 -0500

    Integration test for WebsiteController - create method - complete

--- src/test/java/com/bigname/pim/client/web/controller/WebsiteControllerTest.java
+++ src/test/java/com/bigname/pim/client/web/controller/WebsiteControllerTest.java
@@ -1,17 +1,82 @@
 package com.bigname.pim.client.web.controller;
 
+import com.bigname.common.util.CollectionsUtil;
+import com.bigname.common.util.ConversionUtil;
+import com.bigname.core.util.FindBy;
+import com.bigname.pim.PimApplication;
+import com.bigname.pim.api.domain.User;
+import com.bigname.pim.api.domain.Website;
+import com.bigname.pim.api.persistence.dao.WebsiteDAO;
+import com.bigname.pim.api.service.UserService;
+import com.bigname.pim.api.service.WebsiteService;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+import static com.bigname.common.util.ConversionUtil.*;
 import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 /**
  * Created by sruthi on 23-02-2019.
  */
+/*@RunWith(SpringRunner.class)
+@ActiveProfiles("test")
+@SpringBootTest
+@AutoConfigureMockMvc
+@ContextConfiguration(classes={PimApplication.class})
+@WebAppConfiguration*/
+@ActiveProfiles("test")
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@ContextConfiguration(classes={PimApplication.class})
 public class WebsiteControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    WebsiteController websiteController;
+
+    @Autowired
+    private WebsiteDAO websiteDAO;
+
     @Before
     public void setUp() throws Exception {
+        if(!userService.get("MANU@BLACWOOD.COM", FindBy.EXTERNAL_ID).isPresent()) {
+            User user = new User();
+            user.setUserName("MANU@BLACWOOD.COm");
+            user.setPassword("temppass");
+            user.setEmail("manu@blacwood.com");
+            user.setActive("Y");
+            userService.create(user);
+        }
+        websiteDAO.getMongoTemplate().dropCollection(Website.class);
     }
 
     @After
@@ -19,7 +84,29 @@ public class WebsiteControllerTest {
     }
 
     @Test
+    public void contexLoads() throws Exception {
+        Assert.assertNotNull(websiteController);
+    }
+
+    @WithUserDetails("manu@blacwood.com")
+    @Test
     public void create() throws Exception {
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.put("group", ConversionUtil.toList("CREATE"));
+        params.put("websiteName", ConversionUtil.toList("TestSite.com"));
+        params.put("websiteId", ConversionUtil.toList("TEST"));
+        params.put("url", ConversionUtil.toList("https://www.testsite.com"));
+        ResultActions result = mockMvc.perform(
+            post("/pim/websites")
+                .params(params)
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .accept(MediaType.APPLICATION_JSON_UTF8));
+
+
+                result.andExpect(status().isOk());
+        result.andExpect(jsonPath("$.size()").value(3));
+        result.andExpect(jsonPath("$.success").value(true));
+        result.andExpect(jsonPath("$.group.length()").value(1));
     }
 
     @Test
