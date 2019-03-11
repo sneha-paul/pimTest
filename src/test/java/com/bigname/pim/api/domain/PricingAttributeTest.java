1508a085733592fa577d83a61ee0f7e36be55b32
sanoop9745 <sanoop.p@e-xposures.com>
2019-03-11 11:49:44 +0000

    Domain test class pricingAttribute implementation

--- src/test/java/com/bigname/pim/api/domain/PricingAttributeTest.java
+++ src/test/java/com/bigname/pim/api/domain/PricingAttributeTest.java
@@ -1,6 +1,8 @@
 package com.bigname.pim.api.domain;
 
+import com.bigname.common.util.CollectionsUtil;
 import com.bigname.common.util.ValidationUtil;
+import com.bigname.core.domain.Entity;
 import com.bigname.pim.PimApplication;
 import com.bigname.pim.api.persistence.dao.PricingAttributeDAO;
 import com.bigname.pim.api.service.PricingAttributeService;
@@ -14,6 +16,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 import static com.bigname.core.util.FindBy.EXTERNAL_ID;
 import static org.junit.Assert.*;
@@ -36,12 +42,14 @@ public class PricingAttributeTest {
     }
     @Test
     public void accessorsTest() {
+        //Create new instance
         PricingAttribute pricingAttributeDTO = new PricingAttribute();
         pricingAttributeDTO.setPricingAttributeId("test");
         pricingAttributeDTO.setPricingAttributeName("test");
 
         pricingAttributeDTO.orchestrate();
 
+        //Testing equals with id
         Assert.assertEquals(pricingAttributeDTO.getPricingAttributeId(), "TEST");
         Assert.assertEquals(pricingAttributeDTO.getPricingAttributeName(), "test");
         Assert.assertEquals(pricingAttributeDTO.getActive(), "N");
@@ -56,18 +64,104 @@ public class PricingAttributeTest {
 
     @Test
     public void cloneInstance() throws Exception {
+        //Create New Map
+        List<Map<String,Object>> pricingAttributesData = new ArrayList<>();
+        pricingAttributesData.add(CollectionsUtil.toMap("name", "Test1.com", "externalId", "TEST_1", "active", "Y"));
+
+        pricingAttributesData.forEach(pricingAttributeData -> {
+            PricingAttribute pricingAttributeDTO = new PricingAttribute();
+            pricingAttributeDTO.setPricingAttributeName((String) pricingAttributeData.get("name"));
+            pricingAttributeDTO.setPricingAttributeId((String) pricingAttributeData.get("externalId"));
+            pricingAttributeDTO.setActive((String) pricingAttributeData.get("active"));
+            pricingAttributeDAO.insert(pricingAttributeDTO);
+
+            //Clone PricingAttribute
+            PricingAttribute newPricingAttribute = pricingAttributeService.get(pricingAttributeDTO.getPricingAttributeId(), EXTERNAL_ID, false).orElse(null);
+           Assert.assertTrue(newPricingAttribute != null);
+             Assert.assertTrue(newPricingAttribute.diff(pricingAttributeDTO).isEmpty());
+
+            PricingAttribute pricingAttributeClone = pricingAttributeService.cloneInstance(newPricingAttribute.getPricingAttributeId(), EXTERNAL_ID, Entity.CloneType.LIGHT);
+            Assert.assertTrue(pricingAttributeClone.getPricingAttributeId() .equals(newPricingAttribute.getPricingAttributeId() + "_COPY") && pricingAttributeClone.getPricingAttributeName().equals(newPricingAttribute.getPricingAttributeName() + "_COPY") &&  pricingAttributeClone.getActive() != newPricingAttribute.getActive());
+        });
     }
 
     @Test
     public void merge() throws Exception {
+        //Create Original instance
+        PricingAttribute original = new PricingAttribute();
+        original.setPricingAttributeId("test");
+        original.setExternalId("test");
+        original.setPricingAttributeName("test");
+        original.setActive("N");
+
+        //Modified instance or ADD details
+        PricingAttribute modified = new PricingAttribute();
+        modified.setGroup("DETAILS");
+        modified.setPricingAttributeName("TEST");
+        modified.setPricingAttributeId("TEST");
+        modified.setExternalId("TEST");
+        modified.setActive("N");
+
+        //Merge Original to Modified
+        original = original.merge(modified);
+        Assert.assertEquals(original.getPricingAttributeName(), "TEST");
+        Assert.assertEquals(original.getPricingAttributeId(), "TEST");
+        Assert.assertEquals(original.getExternalId(), "TEST");
+        Assert.assertEquals(original.getActive(), "N");
+
+        //Without Details
+        PricingAttribute modified1 = new PricingAttribute();
+        modified1.setPricingAttributeName("test");
+        modified1.setPricingAttributeId("test");
+        modified1.setExternalId("test");
+        modified1.setActive("N");
+
+        original = original.merge(modified1);
+        Assert.assertEquals(original.getPricingAttributeName(), "TEST");
+        Assert.assertEquals(original.getPricingAttributeId(), "TEST");
+        Assert.assertEquals(original.getExternalId(), "TEST");
+        Assert.assertEquals(original.getActive(), "N");
+
     }
 
     @Test
     public void toMap() throws Exception {
+        //Create new instance
+        PricingAttribute pricingAttributeDTO = new PricingAttribute();
+        pricingAttributeDTO.setPricingAttributeId("test");
+        pricingAttributeDTO.setPricingAttributeName("test");
+        pricingAttributeDTO.setExternalId("test");
+        pricingAttributeDTO.setActive("N");
+
+        Assert.assertTrue(ValidationUtil.isNotEmpty(pricingAttributeDTO.getPricingAttributeId()));
+
+        //Testing equals with id
+        Assert.assertEquals(pricingAttributeDTO.getPricingAttributeId(), "TEST");
+        Assert.assertEquals(pricingAttributeDTO.getPricingAttributeId(), "TEST");
+        Assert.assertEquals(pricingAttributeDTO.getExternalId(), "TEST");
+        Assert.assertEquals(pricingAttributeDTO.getActive(), "N");
+
+        Assert.assertTrue(ValidationUtil.isNotEmpty(pricingAttributeDTO.getPricingAttributeId()));
+        Assert.assertTrue(ValidationUtil.isNotEmpty(pricingAttributeDTO.getExternalId()));
+        Assert.assertTrue(ValidationUtil.isNotEmpty(pricingAttributeDTO.getPricingAttributeName()));
+        Assert.assertTrue(ValidationUtil.isNotEmpty(pricingAttributeDTO.getActive()));
     }
 
     @Test
     public void diff() throws Exception {
+        //Create first instance
+        PricingAttribute pricingAttribute1 = new PricingAttribute();
+        pricingAttribute1.setExternalId("test");
+        pricingAttribute1.setPricingAttributeName("test");
+
+        //Create second instance
+        PricingAttribute pricingAttribute2 = new PricingAttribute();
+        pricingAttribute2.setExternalId("test");
+        pricingAttribute2.setPricingAttributeName("test.com");
+
+        Map<String, Object> diff = pricingAttribute1.diff(pricingAttribute2);
+        Assert.assertEquals(diff.size(), 2);
+        Assert.assertEquals(diff.get("pricingAttributeName"), "test.com");
     }
     @After
     public void tearDown() throws Exception {
