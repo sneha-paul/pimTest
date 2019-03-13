5b55010dc8d3d3080fcb0c18f9085a32922ded07
Aswathy Sadanandan <aswathy.sadanandan@e-xposures.com>
2019-03-13 12:10:58 +0530

    AttributeCollectionServiceImplTest : Completed

--- src/test/java/com/bigname/pim/api/service/impl/AttributeCollectionServiceImplTest.java
+++ src/test/java/com/bigname/pim/api/service/impl/AttributeCollectionServiceImplTest.java
@@ -3,15 +3,13 @@ package com.bigname.pim.api.service.impl;
 import com.bigname.common.util.CollectionsUtil;
 import com.bigname.common.util.ValidationUtil;
 import com.bigname.core.domain.ValidatableEntity;
-import com.bigname.core.exception.EntityNotFoundException;
 import com.bigname.core.util.FindBy;
 import com.bigname.pim.PimApplication;
-import com.bigname.pim.api.domain.Attribute;
-import com.bigname.pim.api.domain.AttributeCollection;
-import com.bigname.pim.api.domain.AttributeGroup;
-import com.bigname.pim.api.domain.AttributeOption;
+import com.bigname.pim.api.domain.*;
 import com.bigname.pim.api.persistence.dao.AttributeCollectionDAO;
+import com.bigname.pim.api.persistence.dao.FamilyDAO;
 import com.bigname.pim.api.service.AttributeCollectionService;
+import com.bigname.pim.api.service.FamilyService;
 import org.javatuples.Pair;
 import org.junit.After;
 import org.junit.Assert;
@@ -32,8 +30,6 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 import static com.bigname.core.util.FindBy.EXTERNAL_ID;
-import static com.bigname.core.util.FindBy.findBy;
-import static org.junit.Assert.*;
 
 /**
  * Created by sruthi on 23-02-2019.
@@ -48,9 +44,14 @@ public class AttributeCollectionServiceImplTest {
 
     @Autowired
     AttributeCollectionDAO attributeCollectionDAO;
+
+    @Autowired
+    FamilyDAO familyDAO;
+
     @Before
     public void setUp() throws Exception {
         attributeCollectionDAO.getMongoTemplate().dropCollection(AttributeCollection.class);
+        familyDAO.getMongoTemplate().dropCollection(Family.class);
     }
 
     @Test
@@ -165,132 +166,148 @@ public class AttributeCollectionServiceImplTest {
 
     @Test
     public void getAttributeOptions() throws Exception {
-      /*  List<Map<String, Object>> attributesCollectionData = new ArrayList<>();
-        attributesCollectionData.add(CollectionsUtil.toMap("name", "Test1.com", "externalId", "TEST_1", "active", "Y"));
+        AttributeCollection attributeCollectionDTO = new AttributeCollection();
+        attributeCollectionDTO.setCollectionName("Test_AttributeCollection");
+        attributeCollectionDTO.setCollectionId("TEST_ATTRIBUTECOLLECTION");
+        attributeCollectionDTO.setActive("Y");
+        attributeCollectionDTO.setDiscontinued("N");
+        attributeCollectionDAO.insert(attributeCollectionDTO);
 
-        attributesCollectionData.forEach(AttributeCollection -> {
-            AttributeCollection attributeCollectionDTO= new AttributeCollection();
-            attributeCollectionDTO.setCollectionName((String)AttributeCollection.get("name"));
-            attributeCollectionDTO.setCollectionId((String)AttributeCollection.get("externalId"));
-            attributeCollectionDTO.setActive((String)AttributeCollection.get("active"));
-            attributeCollectionDAO.insert(attributeCollectionDTO);
+        AttributeCollection attributeCollectionDetails = attributeCollectionDAO.findByExternalId(attributeCollectionDTO.getCollectionId()).orElse(null);
 
-            AttributeCollection attributeCollectionDetails = attributeCollectionDAO.findByExternalId(attributeCollectionDTO.getCollectionId()).orElse(null);
+        Attribute attribute = new Attribute();
+        attribute.setActive("Y");
+        attribute.setAttributeGroup(AttributeGroup.getDefaultGroup());
+        attribute.setUiType(Attribute.UIType.DROPDOWN);
+        attribute.setName("Test_Attribute");
+        attribute.setId("TEST_ATTRIBUTE");
+        attributeCollectionDetails.addAttribute(attribute);
 
-            List<Map<String, Object>> attributesData = new ArrayList<>();
-            attributesData.add(CollectionsUtil.toMap("name", "style", "active", "Y", "id", "STYLE", "uiType", "DROPDOWN"));
-            attributesData.forEach(attributeData -> {
-                Attribute attribute = new Attribute();
-                attribute.setActive((String)attributeData.get("active"));
-                attribute.setAttributeGroup(AttributeGroup.getDefaultGroup());
-                attribute.setName((String)attributeData.get("name"));
-                attribute.setId((String)attributeData.get("id"));
-                attribute.setUiType(Attribute.UIType.DROPDOWN);
-                attributeCollectionDetails.addAttribute(attribute);
+        Optional<Attribute> attributeDetails = attributeCollectionDetails.getAttribute(attribute.getFullId());
+        AttributeOption attributeOption = new AttributeOption();
+        attributeOption.setCollectionId(attributeCollectionDTO.getCollectionId());
+        attributeOption.setValue("TestOption");
+        attributeOption.setAttributeId(attribute.getFullId());
+        attributeOption.setActive("Y");
+        attributeOption.orchestrate();
+        attributeDetails.get().getOptions().put(ValidatableEntity.toId("TestOption"), attributeOption);
 
-                Attribute attribute1 = attributeCollectionDetails.getAttribute(attribute.getFullId()).orElse(null);
+        attributeCollectionDAO.save(attributeCollectionDetails);
 
-                List<Map<String, Object>> attributesOptionsData = new ArrayList<>();
-                attributesOptionsData.add(CollectionsUtil.toMap("value", "FOLDERS", "active", "Y"));
-                attributesOptionsData.add(CollectionsUtil.toMap("value", "OPEN_END", "active", "Y"));
-                attributesOptionsData.add(CollectionsUtil.toMap("value", "PAPER", "active", "Y"));
-                attributesOptionsData.forEach(attributeOptionData ->{
-                    AttributeOption attributeOption = new AttributeOption();
-                    attributeOption.setValue((String)attributeOptionData.get("value"));
-                    attributeOption.setCollectionId(attributeCollectionDetails.getCollectionId());
-                    attributeOption.setActive((String)attributeOptionData.get("active"));
-                    attributeOption.setAttributeId(attribute.getFullId());
-                    attributeOption.orchestrate();
-                    attribute1.getOptions().put(ValidatableEntity.toId(attributeOption.getValue()), attributeOption);
-                });
-            });
-            attributeCollectionDAO.save(attributeCollectionDetails);
-          //  AttributeCollection attributeCollection = attributeCollectionDAO.findById(attributeCollectionDTO.getCollectionId(), FindBy.EXTERNAL_ID).orElse(null);
-            Page<AttributeOption> result = attributeCollectionService.getAttributeOptions(attributeCollectionDTO.getCollectionId(), FindBy.EXTERNAL_ID, "STYLE",0,1,null);
-            System.out.println(result);
-        });*/
+        Page<AttributeOption> result = attributeCollectionService.getAttributeOptions(attributeCollectionDTO.getCollectionId(), FindBy.EXTERNAL_ID, attributeCollectionDTO.getAttributeFullId(attributeOption),0,1,null);
+        Assert.assertEquals(result.getContent().size(), 1);
+        Assert.assertEquals(result.getContent().get(0).getValue(), "TestOption");
+
     }
 
     @Test
     public void findAttribute() throws Exception {
-      /*  List<Map<String, Object>> attributesCollectionData = new ArrayList<>();
-        attributesCollectionData.add(CollectionsUtil.toMap("name", "Test1.com", "externalId", "TEST_1", "active", "Y"));
+        AttributeCollection attributeCollectionDTO = new AttributeCollection();
+        attributeCollectionDTO.setCollectionName("Test_AttributeCollection");
+        attributeCollectionDTO.setCollectionId("TEST_ATTRIBUTECOLLECTION");
+        attributeCollectionDTO.setActive("Y");
+        attributeCollectionDTO.setDiscontinued("N");
+        attributeCollectionDAO.insert(attributeCollectionDTO);
 
-        attributesCollectionData.forEach(AttributeCollection -> {
-            AttributeCollection attributeCollectionDTO= new AttributeCollection();
-            attributeCollectionDTO.setCollectionName((String)AttributeCollection.get("name"));
-            attributeCollectionDTO.setCollectionId((String)AttributeCollection.get("externalId"));
-            attributeCollectionDTO.setActive((String)AttributeCollection.get("active"));
-            attributeCollectionDAO.insert(attributeCollectionDTO);
+        AttributeCollection attributeCollectionDetails = attributeCollectionDAO.findByExternalId(attributeCollectionDTO.getCollectionId()).orElse(null);
 
-            AttributeCollection attributeCollectionDetails = attributeCollectionDAO.findByExternalId(attributeCollectionDTO.getCollectionId()).orElse(null);
+        Attribute attribute = new Attribute();
+        attribute.setActive("Y");
+        attribute.setAttributeGroup(AttributeGroup.getDefaultGroup());
+        attribute.setUiType(Attribute.UIType.DROPDOWN);
+        attribute.setName("Test_Attribute");
+        attribute.setId("TEST_ATTRIBUTE");
+        attributeCollectionDetails.addAttribute(attribute);
 
-            List<Map<String, Object>> attributesData = new ArrayList<>();
-            attributesData.add(CollectionsUtil.toMap("name", "style", "active", "Y"));
-            attributesData.add(CollectionsUtil.toMap("name", "color", "active", "Y"));
-            attributesData.add(CollectionsUtil.toMap("name", "size", "active", "Y"));
-            attributesData.forEach(attributeData -> {
-                Attribute attribute = new Attribute();
-                attribute.setActive((String)attributeData.get("active"));
-                attribute.setAttributeGroup(AttributeGroup.getDefaultGroup());
-                attribute.setName((String)attributeData.get("name"));
-                attributeCollectionDetails.addAttribute(attribute);
-            });
-            attributeCollectionDAO.save(attributeCollectionDetails);
-           // String attributeFullId=attributeCollectionDetails.getAttributes().get("name").getFullId();
-            Optional<Attribute> result = attributeCollectionService.findAttribute(attributeCollectionDTO.getCollectionId(),FindBy.EXTERNAL_ID, null);
-        });*/
+        Optional<Attribute> attributeDetails = attributeCollectionDetails.getAttribute(attribute.getFullId());
+        AttributeOption attributeOption = new AttributeOption();
+        attributeOption.setCollectionId(attributeCollectionDTO.getCollectionId());
+        attributeOption.setValue("TestOption");
+        attributeOption.setAttributeId(attribute.getFullId());
+        attributeOption.setActive("Y");
+        attributeOption.orchestrate();
+        attributeDetails.get().getOptions().put(ValidatableEntity.toId("TestOption"), attributeOption);
+
+        attributeCollectionDAO.save(attributeCollectionDetails);
+
+        Optional<Attribute> result = attributeCollectionService.findAttribute(attributeCollectionDTO.getCollectionId(),FindBy.EXTERNAL_ID, attributeCollectionDTO.getAttributeFullId(attributeOption));
+        Assert.assertEquals(result.get().getName(), "Test_Attribute");
     }
 
     @Test
     public void findAttributeOption() throws Exception {
-        /*List<Map<String, Object>> attributesCollectionData = new ArrayList<>();
-        attributesCollectionData.add(CollectionsUtil.toMap("name", "Test1.com", "externalId", "TEST_1", "active", "Y"));
+        AttributeCollection attributeCollectionDTO = new AttributeCollection();
+        attributeCollectionDTO.setCollectionName("Test_AttributeCollection");
+        attributeCollectionDTO.setCollectionId("TEST_ATTRIBUTECOLLECTION");
+        attributeCollectionDTO.setActive("Y");
+        attributeCollectionDTO.setDiscontinued("N");
+        attributeCollectionDAO.insert(attributeCollectionDTO);
 
-        attributesCollectionData.forEach(AttributeCollection -> {
-            AttributeCollection attributeCollectionDTO = new AttributeCollection();
-            attributeCollectionDTO.setCollectionName((String) AttributeCollection.get("name"));
-            attributeCollectionDTO.setCollectionId((String) AttributeCollection.get("externalId"));
-            attributeCollectionDTO.setActive((String) AttributeCollection.get("active"));
-            attributeCollectionDAO.insert(attributeCollectionDTO);
+        AttributeCollection attributeCollectionDetails = attributeCollectionDAO.findByExternalId(attributeCollectionDTO.getCollectionId()).orElse(null);
 
-            AttributeCollection attributeCollectionDetails = attributeCollectionDAO.findByExternalId(attributeCollectionDTO.getCollectionId()).orElse(null);
+        Attribute attribute = new Attribute();
+        attribute.setActive("Y");
+        attribute.setAttributeGroup(AttributeGroup.getDefaultGroup());
+        attribute.setUiType(Attribute.UIType.DROPDOWN);
+        attribute.setName("Test_Attribute");
+        attribute.setId("TEST_ATTRIBUTE");
+        attributeCollectionDetails.addAttribute(attribute);
 
-            List<Map<String, Object>> attributesData = new ArrayList<>();
-            attributesData.add(CollectionsUtil.toMap("name", "style", "active", "Y", "id", "STYLE", "uiType", "DROPDOWN"));
-            attributesData.forEach(attributeData -> {
-                Attribute attribute = new Attribute();
-                attribute.setActive((String) attributeData.get("active"));
-                attribute.setAttributeGroup(AttributeGroup.getDefaultGroup());
-                attribute.setName((String) attributeData.get("name"));
-                attribute.setId((String) attributeData.get("id"));
-                attribute.setUiType(Attribute.UIType.DROPDOWN);
-                attributeCollectionDetails.addAttribute(attribute);
+        Optional<Attribute> attributeDetails = attributeCollectionDetails.getAttribute(attribute.getFullId());
+        AttributeOption attributeOption = new AttributeOption();
+        attributeOption.setCollectionId(attributeCollectionDTO.getCollectionId());
+        attributeOption.setValue("TestOption");
+        attributeOption.setAttributeId(attribute.getFullId());
+        attributeOption.setActive("Y");
+        attributeOption.orchestrate();
+        attributeDetails.get().getOptions().put(ValidatableEntity.toId("TestOption"), attributeOption);
 
-                Attribute attribute1 = attributeCollectionDetails.getAttribute(attribute.getFullId()).orElse(null);
+        attributeCollectionDAO.save(attributeCollectionDetails);
 
-                List<Map<String, Object>> attributesOptionsData = new ArrayList<>();
-                attributesOptionsData.add(CollectionsUtil.toMap("value", "FOLDERS", "active", "Y"));
-                attributesOptionsData.add(CollectionsUtil.toMap("value", "OPEN_END", "active", "Y"));
-                attributesOptionsData.add(CollectionsUtil.toMap("value", "PAPER", "active", "Y"));
-                attributesOptionsData.forEach(attributeOptionData -> {
-                    AttributeOption attributeOption = new AttributeOption();
-                    attributeOption.setValue((String) attributeOptionData.get("value"));
-                    attributeOption.setCollectionId(attributeCollectionDetails.getCollectionId());
-                    attributeOption.setActive((String) attributeOptionData.get("active"));
-                    attributeOption.setAttributeId(attribute.getFullId());
-                    attributeOption.orchestrate();
-                    attribute1.getOptions().put(ValidatableEntity.toId(attributeOption.getValue()), attributeOption);
-                });
-            });
-            attributeCollectionDAO.save(attributeCollectionDetails);
-           // String attributeOptionId=;
-        });*/
+        List<Map<String, Object>> familiesData = new ArrayList<>();
+        familiesData.add(CollectionsUtil.toMap("name", "Test1", "externalId", "TEST_1", "active", "Y", "discontinue", "N"));
+
+        familiesData.forEach(familyData -> {
+            AttributeCollection finalAttributeCollectionDetails = attributeCollectionDAO.findByExternalId(attributeCollectionDTO.getCollectionId()).orElse(null);
+            Family familyDTO = new Family();
+            familyDTO.setFamilyName((String)familyData.get("name"));
+            familyDTO.setFamilyId((String)familyData.get("externalId"));
+            familyDTO.setActive((String)familyData.get("active"));
+            familyDTO.setDiscontinued((String)familyData.get("discontinue"));
+            familyDAO.insert(familyDTO);
+
+            Family family = familyDAO.findByExternalId(familyDTO.getFamilyId()).orElse(null);
+            Assert.assertTrue(family != null);
+
+            FamilyAttributeGroup familyAttributeGroup = new FamilyAttributeGroup();
+            familyAttributeGroup.setActive("Y");
+            familyAttributeGroup.setMasterGroup("Y");
+            familyAttributeGroup.setName(FamilyAttributeGroup.DEFAULT_GROUP);
+            familyAttributeGroup.setId(familyAttributeGroup.getFullId());
+
+            //Create the new familyAttribute instance
+            FamilyAttribute familyAttributeDTO = new FamilyAttribute(attribute.getName(), null);
+            familyAttributeDTO.setActive("Y");
+            familyAttributeDTO.setCollectionId(finalAttributeCollectionDetails.getCollectionId());
+            familyAttributeDTO.setUiType(attribute.getUiType());
+            familyAttributeDTO.setScopable("Y");
+            familyAttributeDTO.setAttributeId(attribute.getFullId());
+            familyAttributeDTO.getScope().put("ECOMMERCE", FamilyAttribute.Scope.OPTIONAL);
+            familyAttributeDTO.setAttributeGroup(familyAttributeGroup); //TODO : check whether right or wrong
+            familyAttributeDTO.setAttribute(attribute);
+
+            family.addAttribute(familyAttributeDTO);
+            familyDAO.save(family);
+
+            Optional<AttributeOption> result= attributeCollectionService.findAttributeOption(familyAttributeDTO,"TESTOPTION");
+            Assert.assertEquals(result.get().getValue(), "TestOption");
+        });
     }
 
     @After
     public void tearDown() throws Exception {
         attributeCollectionDAO.getMongoTemplate().dropCollection(AttributeCollection.class);
+        familyDAO.getMongoTemplate().dropCollection(Family.class);
     }
 
 }
