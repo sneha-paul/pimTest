cc4076349b0f0305b9930ea3e3ab709116c7b6a1
Sruthi Krishna <Sruthi.Krishna@e-xposures.com>
2019-03-13 17:39:26 +0530

    ProductVariantRepositoryTest - createProductVariant

--- src/test/java/com/bigname/pim/api/persistence/dao/ProductVariantRepositoryTest.java
+++ src/test/java/com/bigname/pim/api/persistence/dao/ProductVariantRepositoryTest.java
@@ -2,11 +2,12 @@ package com.bigname.pim.api.persistence.dao;
 
 import com.bigname.common.util.CollectionsUtil;
 import com.bigname.common.util.ConversionUtil;
+import com.bigname.common.util.ValidationUtil;
+import com.bigname.core.domain.ValidatableEntity;
 import com.bigname.core.util.FindBy;
 import com.bigname.pim.PimApplication;
-import com.bigname.pim.api.domain.Family;
-import com.bigname.pim.api.domain.Product;
-import com.bigname.pim.api.domain.ProductVariant;
+import com.bigname.pim.api.domain.*;
+import com.bigname.pim.api.service.AttributeCollectionService;
 import com.bigname.pim.util.PimUtil;
 import org.junit.After;
 import org.junit.Assert;
@@ -22,10 +23,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 import java.util.stream.Collectors;
 
 /**
@@ -38,25 +36,195 @@ import java.util.stream.Collectors;
 public class ProductVariantRepositoryTest {
 
     @Autowired
-    ProductVariantDAO productVariantDAO;
+    private ProductVariantDAO productVariantDAO;
+
     @Autowired
-    ProductDAO productDAO;
+    private ProductDAO productDAO;
+
     @Autowired
-    FamilyDAO familyDAO;
+    private FamilyDAO familyDAO;
+
+    @Autowired
+    private AttributeCollectionDAO attributeCollectionDAO;
+
+    @Autowired
+    private ChannelDAO channelDAO;
+
+    @Autowired
+    private AttributeCollectionService attributeCollectionService;
 
     @Before
     public void setUp() {
         productVariantDAO.getMongoTemplate().dropCollection(ProductVariant.class);
+
+        productDAO.getMongoTemplate().dropCollection(Product.class);
+
+        familyDAO.getMongoTemplate().dropCollection(Family.class);
+
+        attributeCollectionDAO.getMongoTemplate().dropCollection(AttributeCollection.class);
+
+        channelDAO.getMongoTemplate().dropCollection(Channel.class);
     }
 
     @Test
     public void createProductVariantTest() {
+        List<Map<String, Object>> channelsData = new ArrayList<>();
+        channelsData.add(CollectionsUtil.toMap("name", "Ecommerce", "externalId", "ECOMMERCE", "active", "Y"));
+
+        channelsData.forEach(channelData -> {
+            Channel channel = new Channel();
+            channel.setChannelName((String)channelData.get("name"));
+            channel.setChannelId((String)channelData.get("externalId"));
+            channel.setActive((String)channelData.get("active"));
+            channelDAO.insert(channel);
+        });
+
+        Channel channel = channelDAO.findById(channelsData.get(0).get("externalId").toString(), FindBy.EXTERNAL_ID).orElse(null);
+        Assert.assertTrue(ValidationUtil.isNotEmpty(channel));
+
+        AttributeCollection attributeCollectionDTO = new AttributeCollection();
+        attributeCollectionDTO.setCollectionName("Test_AttributeCollection");
+        attributeCollectionDTO.setCollectionId("TEST_ATTRIBUTECOLLECTION");
+        attributeCollectionDTO.setActive("Y");
+        attributeCollectionDTO.setDiscontinued("N");
+        attributeCollectionDAO.insert(attributeCollectionDTO);
+
+        AttributeCollection attributeCollectionDetails = attributeCollectionDAO.findByExternalId(attributeCollectionDTO.getCollectionId()).orElse(null);
+        Assert.assertTrue(ValidationUtil.isNotEmpty(attributeCollectionDetails));
+
+        List<Map<String, Object>> attributesData = new ArrayList<>();
+        attributesData.add(CollectionsUtil.toMap("name", "Color", "externalId", "COLOR", "active", "Y", "uiType", Attribute.UIType.DROPDOWN));
+        attributesData.add(CollectionsUtil.toMap("name", "TestAttribute2", "externalId", "TEST_ATTRIBUTE_2", "active", "Y", "uiType", Attribute.UIType.INPUT_BOX));
+        attributesData.add(CollectionsUtil.toMap("name", "TestAttribute3", "externalId", "TEST_ATTRIBUTE_3", "active", "Y", "uiType", Attribute.UIType.DROPDOWN));
+        attributesData.add(CollectionsUtil.toMap("name", "TestAttribute4", "externalId", "TEST_ATTRIBUTE_4", "active", "Y", "uiType", Attribute.UIType.TEXTAREA));
+        attributesData.add(CollectionsUtil.toMap("name", "TestAttribute5", "externalId", "TEST_ATTRIBUTE_5", "active", "Y", "uiType", Attribute.UIType.INPUT_BOX));
+        attributesData.add(CollectionsUtil.toMap("name", "TestAttribute6", "externalId", "TEST_ATTRIBUTE_6", "active", "Y", "uiType", Attribute.UIType.TEXTAREA));
+        attributesData.add(CollectionsUtil.toMap("name", "TestAttribute7", "externalId", "TEST_ATTRIBUTE_7", "active", "Y", "uiType", Attribute.UIType.TEXTAREA));
+        attributesData.add(CollectionsUtil.toMap("name", "TestAttribute8", "externalId", "TEST_ATTRIBUTE_8", "active", "Y", "uiType", Attribute.UIType.INPUT_BOX));
+        attributesData.add(CollectionsUtil.toMap("name", "TestAttribute9", "externalId", "TEST_ATTRIBUTE_9", "active", "Y", "uiType", Attribute.UIType.INPUT_BOX));
+        attributesData.add(CollectionsUtil.toMap("name", "TestAttribute10", "externalId", "TEST_ATTRIBUTE_10", "active", "Y", "uiType", Attribute.UIType.DROPDOWN));
+
+        attributesData.forEach(attributeData -> {
+            Attribute attribute = new Attribute();
+            attribute.setAttributeGroup(AttributeGroup.getDefaultGroup());
+            attribute.setUiType((Attribute.UIType) attributeData.get("uiType"));
+            attribute.setName((String) attributeData.get("name"));
+            attribute.setId((String) attributeData.get("externalId"));
+            attribute.setActive((String) attributeData.get("active"));
+            attributeCollectionDetails.addAttribute(attribute);
+        });
+
+        attributeCollectionDAO.save(attributeCollectionDetails);
+
+        AttributeCollection attributeCollection = attributeCollectionService.get(attributeCollectionDetails.getCollectionId(), FindBy.EXTERNAL_ID, false).orElse(null);
+
+        List<Attribute> attributes = attributeCollection.getAllAttributes();
+        Attribute attributeDetails = attributeCollectionDetails.getAttribute(attributes.get(0).getFullId()).orElse(null);
+
+        List<Map<String, Object>> attributeOptionsData = new ArrayList<>();
+        attributeOptionsData.add(CollectionsUtil.toMap("value", "Blue", "active", "Y"));
+        attributeOptionsData.add(CollectionsUtil.toMap("value", "Green", "active", "Y"));
+        attributeOptionsData.add(CollectionsUtil.toMap("value", "Red", "active", "Y"));
+
+        attributeOptionsData.forEach(attributeOptionData -> {
+            AttributeOption attributeOption = new AttributeOption();
+            attributeOption.setCollectionId(attributeCollectionDTO.getCollectionId());
+            attributeOption.setValue((String) attributeOptionData.get("value"));
+            attributeOption.setAttributeId(attributeDetails.getFullId());
+            attributeOption.setActive((String) attributeOptionData.get("active"));
+            attributeOption.orchestrate();
+            attributeDetails.getOptions().put(ValidatableEntity.toId(attributeOption.getValue()), attributeOption);
+        });
+
+        attributeCollectionDAO.save(attributeCollectionDetails);
+
+        List<Map<String, Object>> familiesData = new ArrayList<>();
+        familiesData.add(CollectionsUtil.toMap("name", "Test1", "externalId", "TEST_1", "active", "Y", "discontinue", "N"));
+
+        familiesData.forEach((Map<String, Object> familyData) -> {
+            AttributeCollection finalAttributeCollectionDetails = attributeCollectionDAO.findByExternalId(attributeCollectionDTO.getCollectionId()).orElse(null);
+            Family familyDTO = new Family();
+            familyDTO.setFamilyName((String)familyData.get("name"));
+            familyDTO.setFamilyId((String)familyData.get("externalId"));
+            familyDTO.setActive((String)familyData.get("active"));
+            familyDTO.setDiscontinued((String)familyData.get("discontinue"));
+            familyDAO.insert(familyDTO);
+
+            Family family = familyDAO.findByExternalId(familyDTO.getFamilyId()).orElse(null);
+            Assert.assertTrue(ValidationUtil.isNotEmpty(family));
+
+            FamilyAttributeGroup familyAttributeGroup = new FamilyAttributeGroup();
+            familyAttributeGroup.setActive("Y");
+            familyAttributeGroup.setMasterGroup("Y");
+            familyAttributeGroup.setName(FamilyAttributeGroup.DEFAULT_GROUP);
+            familyAttributeGroup.setId(familyAttributeGroup.getFullId());
+
+            //Create the new familyAttribute instance
+            FamilyAttribute familyAttributeDTO = new FamilyAttribute(attributeDetails.getName(), null);
+            familyAttributeDTO.setActive("Y");
+            familyAttributeDTO.setCollectionId(finalAttributeCollectionDetails.getCollectionId());
+            familyAttributeDTO.setUiType(attributeDetails.getUiType());
+            familyAttributeDTO.setScopable("Y");
+            familyAttributeDTO.setAttributeId(attributeDetails.getFullId());
+            familyAttributeDTO.getScope().put(channel.getChannelId(), FamilyAttribute.Scope.OPTIONAL);
+            familyAttributeDTO.setAttributeGroup(familyAttributeGroup);
+            familyAttributeDTO.setAttribute(attributeDetails);
+
+            family.addAttribute(familyAttributeDTO);
+
+            FamilyAttribute familyAttribute = family.getAllAttributesMap(false).get(attributeDetails.getId());
+
+            List<AttributeOption> attributeOptionList = new ArrayList(attributeDetails.getOptions().values());
+            attributeOptionList.forEach(attributeOption -> {
+                FamilyAttributeOption familyAttributeOption = new FamilyAttributeOption();
+                familyAttributeOption.setActive("Y");
+                familyAttributeOption.setValue(attributeOption.getValue());
+                familyAttributeOption.setId(attributeOption.getId());
+                familyAttributeOption.setFamilyAttributeId(familyAttribute.getId());
+                familyAttributeDTO.getOptions().put(attributeOption.getId(), familyAttributeOption);
+                family.addAttributeOption(familyAttributeOption, attributeOption);
+            });
+
+            //set parentAttribute //TODO
+
+            //create variantGroup
+            family.setGroup("VARIANT_GROUPS");
+            VariantGroup variantGroup = new VariantGroup();
+            variantGroup.setName(attributeDetails.getName());
+            variantGroup.setId(attributeDetails.getId());
+            variantGroup.setActive("Y");
+            variantGroup.setLevel(1);
+            variantGroup.setFamilyId(family.getFamilyId());
+            variantGroup.getVariantAxis().put(1, Arrays.asList(attributeDetails.getName()));
+            variantGroup.getVariantAttributes().put(1, Arrays.asList(attributes.get(2).getName(), attributes.get(9).getName()));
+            family.addVariantGroup(variantGroup);
+            family.getChannelVariantGroups().put(channel.getChannelId(), variantGroup.getId());
+
+            familyDAO.save(family);
+
+        });
+
+        Family familyDetails = familyDAO.findByExternalId(familiesData.get(0).get("externalId").toString()).orElse(null);
+
+        //create Product instance
+        Product productDTO = new Product();
+        productDTO.setProductName("Test1");
+        productDTO.setProductId("TEST1");
+        productDTO.setChannelId(channel.getChannelId());
+        productDTO.setProductFamilyId(familyDetails.getId());
+        productDTO.setActive("Y");
+        Product product = productDAO.insert(productDTO);
+        Assert.assertTrue(product.diff(productDTO).isEmpty());
+
+        //create productVariantInstance
+        Product newProduct = productDAO.findById(product.getProductId(), FindBy.EXTERNAL_ID).orElse(null);
+
         ProductVariant productVariantDTO = new ProductVariant();
         productVariantDTO.setProductVariantName("Test1");
         productVariantDTO.setProductVariantId("TEST1");
-        productVariantDTO.setProductId("11a8377c-6a97-49b8-b72a-5f98217e9b88");
+        productVariantDTO.setProductId(newProduct.getId());
         productVariantDTO.setActive("Y");
-        productVariantDTO.setChannelId("ECOMMERCE");
+        productVariantDTO.setChannelId(channel.getChannelId());
         ProductVariant productVariant = productVariantDAO.insert(productVariantDTO);
         Assert.assertTrue(productVariant.diff(productVariantDTO).isEmpty());
 
@@ -257,5 +425,13 @@ public class ProductVariantRepositoryTest {
     @After
     public void tearDown() {
         productVariantDAO.getMongoTemplate().dropCollection(ProductVariant.class);
+
+        productDAO.getMongoTemplate().dropCollection(Product.class);
+
+        familyDAO.getMongoTemplate().dropCollection(Family.class);
+
+        attributeCollectionDAO.getMongoTemplate().dropCollection(AttributeCollection.class);
+
+        channelDAO.getMongoTemplate().dropCollection(Channel.class);
     }
 }
