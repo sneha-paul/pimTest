af67b1a2198d665a2b9b74eb53540cdc348b2124
sanoop9745 <sanoop.p@e-xposures.com>
2019-03-18 13:05:10 +0000

    Domain test class channel methods

--- src/test/java/com/bigname/pim/api/domain/ChannelTest.java
+++ src/test/java/com/bigname/pim/api/domain/ChannelTest.java
@@ -15,6 +15,9 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static com.bigname.core.util.FindBy.EXTERNAL_ID;
 import static org.junit.Assert.*;
 
@@ -38,12 +41,14 @@ public class ChannelTest {
     }
     @Test
     public void accessorsTest(){
+        //Create New Instance
         Channel channelDTO = new Channel();
         channelDTO.setChannelId("test");
         channelDTO.setChannelName("test");
 
         channelDTO.orchestrate();
 
+        //Equals checking With Id
         Assert.assertEquals(channelDTO.getChannelId(), "TEST");
         Assert.assertEquals(channelDTO.getChannelName(), "test");
         Assert.assertEquals(channelDTO.getActive(), "N");
@@ -57,14 +62,70 @@ public class ChannelTest {
     
     @Test
     public void orchestrate() throws Exception {
+        //Create New Instance
+        Channel channelDTO = new Channel();
+        channelDTO.setActive("N");
+       channelDTO.orchestrate();
+
+       //Equals Checking
+        Assert.assertTrue(ValidationUtil.isNotEmpty(channelDTO.getActive()));
+        Assert.assertEquals(channelDTO.getActive(), "N");
     }
 
     @Test
     public void merge() throws Exception {
+        //Create Original Instance
+        Channel original = new Channel();
+        original.setChannelName("Test");
+        original.setActive("Y");
+        original.setDiscontinued("Y");
+
+        //Modified Instance Add Details
+        Channel modified = new Channel();
+        modified.setGroup("DETAILS");
+        modified.setChannelName("Test-A");
+        modified.setActive("N");
+        modified.setDiscontinued("N");
+
+        original = original.merge(modified);
+        Assert.assertEquals(original.getChannelName(), "Test-A");
+        Assert.assertEquals(original.getActive(), "N");
+        Assert.assertEquals(original.getDiscontinued(), "N");
+
+        //Modified Instance Without Details
+        Channel modified1 = new Channel();
+        modified1.setChannelName("test");
+        modified1.setActive("Y");
+        modified1.setDiscontinued("Y");
+
+        original = original.merge(modified1);
+        Assert.assertEquals(original.getChannelName(), "Test-A");
+        Assert.assertEquals(original.getActive(), "N");
+        Assert.assertEquals(original.getDiscontinued(), "N");
     }
 
     @Test
     public void toMap() throws Exception {
+        //Create new instance
+        Channel channelDTO = new Channel();
+        channelDTO.setChannelName("TEST");
+        channelDTO.setExternalId("test");
+        channelDTO.setActive("Y");
+        channelDTO.setDiscontinued("Y");
+
+        Map<String, String> map = new HashMap<>();
+        map.put("channelName", "TEST");
+        map.put("externalId", "TEST");
+        map.put("active", "Y");
+        map.put("discontinued", "Y");
+
+        //Checking Equals for Convert map
+        Map<String, String> map1 = channelDTO.toMap();
+        Assert.assertEquals(map1.get("channelName"), map.get("channelName"));
+        Assert.assertEquals(map1.get("externalId"), map.get("externalId"));
+        Assert.assertEquals(map1.get("active"), map.get("active"));
+        Assert.assertEquals(map1.get("discontinued"), map.get("discontinued"));
+
     }
     @After
     public void tearDown() throws Exception {
