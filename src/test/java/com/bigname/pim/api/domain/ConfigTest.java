257481db2c2eba610c219a1fad02689dd402b200
sanoop9745 <sanoop.p@e-xposures.com>
2019-03-19 13:18:13 +0000

    Config basic test domain class

--- src/test/java/com/bigname/pim/api/domain/ConfigTest.java
+++ src/test/java/com/bigname/pim/api/domain/ConfigTest.java
@@ -15,6 +15,9 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static com.bigname.core.util.FindBy.EXTERNAL_ID;
 import static org.junit.Assert.*;
 
@@ -37,12 +40,14 @@ public class ConfigTest {
     }
     @Test
     public void accessorsTest(){
+        //Create Instance
         Config configDTO = new Config();
         configDTO.setConfigId("test");
         configDTO.setConfigName("test");
 
         configDTO.orchestrate();
 
+        //Equals Checking With Id
         Assert.assertEquals(configDTO.getConfigId(), "TEST");
         Assert.assertEquals(configDTO.getConfigName(), "test");
         Assert.assertEquals(configDTO.getActive(), "N");
@@ -60,10 +65,57 @@ public class ConfigTest {
 
     @Test
     public void merge() throws Exception {
+        //Create Original Instance
+        Config original = new Config();
+        original.setExternalId("test");
+        original.setConfigId("test");
+        original.setConfigName("Test");
+
+        //ADD DETAILS
+        Config modified = new Config();
+        modified.setGroup("DETAILS");
+        modified.setExternalId("TEST-A");
+        modified.setConfigId("test-A");
+        modified.setConfigName("Test-A");
+
+        original = original.merge(modified);
+        Assert.assertEquals(original.getExternalId(), "TEST-A");
+        Assert.assertEquals(original.getConfigId(), "TEST-A");
+        Assert.assertEquals(original.getConfigName(), "Test-A");
+
+        //Without DETAILS
+        Config modified1 = new Config();
+        modified1.setExternalId("test");
+        modified1.setConfigId("test");
+        modified1.setConfigName("test");
+
+        original = original.merge(modified1);
+        Assert.assertEquals(original.getExternalId(), "TEST-A");
+        Assert.assertEquals(original.getConfigId(), "TEST-A");
+        Assert.assertEquals(original.getConfigName(), "Test-A");
     }
 
     @Test
     public void toMap() throws Exception {
+        //Create New Instance
+        Config configDTO = new Config();
+        configDTO.setExternalId("test");
+        configDTO.setConfigName("Test");
+        configDTO.setActive("Y");
+        configDTO.setDiscontinued("Y");
+
+        //Equals Checking For Map
+        Map<String, String> map = new HashMap<>();
+        map.put("externalId", "TEST");
+        map.put("configName", "Test");
+        map.put("active", "Y");
+        map.put("discontinued", "Y");
+
+        Map<String, String> map1 = configDTO.toMap();
+        Assert.assertEquals(map1.get("externalId"), map.get("externalId"));
+        Assert.assertEquals(map1.get("configName"), map.get("configName"));
+        Assert.assertEquals(map1.get("active"), map.get("active"));
+        Assert.assertEquals(map1.get("discontinued"), map.get("discontinued"));
     }
     @After
     public void tearDown() throws Exception {
