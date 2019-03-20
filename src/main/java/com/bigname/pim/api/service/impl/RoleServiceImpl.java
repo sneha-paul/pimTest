0d62cf77f286905bf6c2c8d7f6ef0283d1ed7b3f
sanoop9745 <sanoop.p@e-xposures.com>
2019-03-20 13:12:41 +0000

    Role service impl refactor

--- src/main/java/com/bigname/pim/api/service/impl/RoleServiceImpl.java
+++ src/main/java/com/bigname/pim/api/service/impl/RoleServiceImpl.java
@@ -6,6 +6,7 @@ import com.bigname.pim.api.domain.Role;
 import com.bigname.pim.api.persistence.dao.RoleDAO;
 import com.bigname.pim.api.service.RoleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import javax.validation.Validator;
 
@@ -13,7 +14,7 @@ import javax.validation.Validator;
 /**
  * Created by sruthi on 27-02-2019.
  */
-
+@Service
 public class RoleServiceImpl extends BaseServiceSupport<Role, RoleDAO, RoleService> implements RoleService {
     private RoleDAO roleDAO;
 
