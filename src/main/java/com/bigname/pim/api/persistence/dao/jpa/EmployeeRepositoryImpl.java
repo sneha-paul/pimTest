d88c2c741f32dcae6a119cf26b8e3b8df8313134
Sruthi Krishna <Sruthi.Krishna@e-xposures.com>
2019-08-01 12:00:07 +0530

    Change in EmployeeRepositoryImpl

--- src/main/java/com/bigname/pim/api/persistence/dao/jpa/EmployeeRepositoryImpl.java
+++ src/main/java/com/bigname/pim/api/persistence/dao/jpa/EmployeeRepositoryImpl.java
@@ -18,49 +18,4 @@ public class EmployeeRepositoryImpl extends GenericRepositoryImpl<Employee, Crit
     public EmployeeRepositoryImpl(JdbcTemplate jdbcTemplate, EntityManager entityManager) {
         super(jdbcTemplate, Employee.class, entityManager);
     }
-
-    @Override
-    public <I> Optional<Employee> findById(ID<I> id, boolean... activeRequired) {
-        return Optional.empty();
-    }
-
-    @Override
-    public List<Employee> findAll(Map<String, Object> criteria, boolean... activeRequired) {
-        return null;
-    }
-
-    @Override
-    public List<Employee> findAll(GenericCriteria<Criteria> criteria, boolean... activeRequired) {
-        return null;
-    }
-
-    @Override
-    public Optional<Employee> findOne(Map<String, Object> criteria, boolean... activeRequired) {
-        return Optional.empty();
-    }
-
-    @Override
-    public <I> Page<Employee> findByIdNotIn(List<ID<I>> ids, Pageable pageable, boolean... activeRequired) {
-        return null;
-    }
-
-    @Override
-    public <I> Page<Employee> findByIdIn(List<ID<I>> ids, Pageable pageable, boolean... activeRequired) {
-        return null;
-    }
-
-    @Override
-    public <I> List<Employee> findByIdIn(List<ID<I>> ids, boolean... activeRequired) {
-        return null;
-    }
-
-    @Override
-    public List<Employee> findByExternalIdStartingWith(ID id, boolean... activeRequired) {
-        return null;
-    }
-
-    @Override
-    public long countById(boolean... activeRequired) {
-        return 0;
-    }
 }
