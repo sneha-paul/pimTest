f47ed561739d9e9324737e86424fd05fce76dc4e
Manu NarayanaPrasad <mnprasad78@gmail.com>
2020-01-27 23:51:36 -0500

    Config - WIP

--- src/main/java/com/bigname/pim/client/web/controller/ConfigController.java
+++ src/main/java/com/bigname/pim/client/web/controller/ConfigController.java
@@ -5,9 +5,11 @@ import com.bigname.pim.core.service.ConfigService;
 import com.bigname.pim.core.util.BreadcrumbsBuilder;
 import com.m7.xtreme.common.datatable.model.Result;
 import com.m7.xtreme.common.util.CollectionsUtil;
+import com.m7.xtreme.common.util.ValidationUtil;
 import com.m7.xtreme.xcore.exception.EntityNotFoundException;
 import com.m7.xtreme.xcore.util.ID;
 import com.m7.xtreme.xcore.web.controller.BaseController;
+import org.javatuples.Pair;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
@@ -30,36 +32,6 @@ public class ConfigController extends BaseController<Config, ConfigService> {
         this.configService = configService;
     }
 
-    //create channel
-    @RequestMapping(method = RequestMethod.POST)
-    @ResponseBody
-    public Map<String, Object> create(Config config) {
-        Map<String, Object> model = new HashMap<>();
-        if (isValid(config, model, Config.CreateGroup.class)) {
-            config.setActive("Y");
-            configService.create(config);
-            model.put("success", true);
-        }
-        return model;
-    }
-
-    //update channel
-    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
-    @ResponseBody
-    public Map<String, Object> update(@PathVariable(value = "id") String id, Config config) {
-        Map<String, Object> model = new HashMap<>();
-        model.put("context", CollectionsUtil.toMap("id", id));
-        if (isValid(config, model, config.getGroup().length == 1 && config.getGroup()[0].equals("DETAILS") ? Config.DetailsGroup.class : null)) {
-            configService.update(ID.EXTERNAL_ID(id), config);
-            model.put("success", true);
-            if (!id.equals(config.getConfigId())) {
-                model.put("refreshUrl", "/pim/configs/" + config.getConfigId());
-            }
-        }
-        return model;
-    }
-
-    // details channel
     @RequestMapping(value = {"/{id}", "/create"})
     public ModelAndView details(@PathVariable(value = "id", required = false) String id,
                                 @RequestParam(name = "reload", required = false) boolean reload) {
@@ -76,8 +48,25 @@ public class ConfigController extends BaseController<Config, ConfigService> {
                 }).orElseThrow(() -> new EntityNotFoundException("Unable to find Config with Id: " + id));
     }
 
+    @RequestMapping(method = RequestMethod.POST)
+    @ResponseBody
+    public Map<String, Object> create(Config config) {
+        Map<String, Object> model = new HashMap<>();
+        if (isValid(config, model, Config.CreateGroup.class)) {
+            config.setActive("Y");
+            configService.create(config);
+            model.put("success", true);
+        }
+        return model;
+    }
 
-    //find all model and view
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    @ResponseBody
+    public Map<String, Object> update(@PathVariable(value = "id") String configId,
+                                      Config config) {
+        return update(configId, config, "/pim/configs/", config.getGroup().length == 1 && config.getGroup()[0].equals("DETAILS") ? Config.DetailsGroup.class : null);
+    }
+
     @RequestMapping()
     public ModelAndView all() {
         Map<String, Object> model = new HashMap<>();
@@ -85,11 +74,35 @@ public class ConfigController extends BaseController<Config, ConfigService> {
         return new ModelAndView("config/configs", model);
     }
 
-    @RequestMapping(value = "/data")
+    @RequestMapping(value = {"/list", "/data"})
     @ResponseBody
     @SuppressWarnings("unchecked")
     public Result<Map<String, String>> all(HttpServletRequest request) {
         return super.all(request, "configName");
+    }
+
+    @RequestMapping(value= {"/{configId}/params/{paramName}", "/{configId}/params/create"})
+    public ModelAndView paramDetails(@PathVariable(value = "configId") String configId,
+                                         @PathVariable(value = "paramName", required = false) String paramName) {
+        String[] paramNames = ValidationUtil.isNotEmpty(paramName) ? paramName.split("\\|") : new String[0];
+        return configService.get(ID.EXTERNAL_ID(configId), false)
+                .map(config -> {
+                    Map<String, Object> model = new HashMap<>();
+                    Map<String, String> param = new HashMap<>();
+                    String mode;
+                    if(paramNames.length == 2) {
+                        mode = "DETAILS";
+                        param.put("name", paramNames[1]);
+                        param.put("scope", paramNames[0]);
+                        param.put("value", config.getParameter(paramNames[1], String.class, paramNames[0]));
+                    } else {
+                        mode = "CREATE";
+                    }
+                    model.put("param", param);
+                    model.put("mode", mode);
+                    return new ModelAndView("config/param", model);
+                }).orElseThrow(() -> new EntityNotFoundException("Unable to find Config with Id: " + configId));
+
     }
 
     /*@RequestMapping("/{id}/params/data")
